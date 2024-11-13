a0157ecca09f0e19f3ad23e03891d3a3e4aba16d
ibanos90 <33061897+ibanos90@users.noreply.github.com>
2024-11-13 12:32:44 -0700

    Pass height in var_z variable for GNSSRO observations assimilation and update all ref files (#1025). Also conflicts resolved
    TYPE: bug fix
    
    DESCRIPTION OF CHANGES:
    This PR changes the MPAS-A variable that is passed in `var_z` as geopotential height for the forward modeling of GNSSRO bending angle and refractivity observations. The change is basically to pass `height` at half levels instead of converting the height to `geopotential height` and passing the latter. By doing this, a known bias in bending angle and refractivity simulations using MPAS-A is totally removed.
    
    This PR also updates the reference files due to the update in refractivity or bending angle simulation/assimilation and also due to recent changes in other repos.
    
    LIST OF MODIFIED FILES:
    **Directly related to changes in this PR**
    M       src/mpasjedi/VariableChange/Model2GeoVars/mpasjedi_vc_model2geovars_mod.F90
    M       test/testoutput/3denvar_2stream_bumploc.ref
    M       test/testoutput/3denvar_bumploc.ref
    M       test/testoutput/3denvar_dual_resolution.ref
    M       test/testoutput/3dfgat.ref
    M       test/testoutput/3dfgat_pseudo.ref
    M       test/testoutput/3dhybrid_bumpcov_bumploc.ref
    M       test/testoutput/3dvar.ref
    M       test/testoutput/3dvar_bumpcov.ref
    M       test/testoutput/3dvar_bumpcov_nbam.ref
    M       test/testoutput/3dvar_bumpcov_ropp.ref
    M       test/testoutput/3dvar_bumpcov_rttovcpp.ref
    M       test/testoutput/4denvar_ID.ref
    M       test/testoutput/4denvar_VarBC.ref
    M       test/testoutput/4denvar_bumploc.ref
    M       test/testoutput/4dfgat.ref
    M       test/testoutput/4dfgat_append_obs.ref
    M       test/testoutput/4dhybrid_bumpcov_bumploc.ref
    M       test/testoutput/eda_3dhybrid.ref
    M       test/testoutput/hofx3d.ref
    M       test/testoutput/hofx3d_nbam.ref
    M       test/testoutput/hofx3d_ropp.ref
    M       test/testoutput/hofx3d_rttovcpp.ref
    M       test/testoutput/hofx4d.ref
    M       test/testoutput/hofx4d_pseudo.ref
    M       test/testoutput/letkf_3dloc.ref
    M       test/testoutput/lgetkf.ref
    M       test/testoutput/lgetkf_height_vloc.ref
    
    **Potentially due to changes in other repositories**
    M       test/testoutput/3denvar_amsua_allsky.ref
    M       test/testoutput/3denvar_amsua_bc.ref
    M       test/testoutput/convertstate.ref
    M       test/testoutput/converttostructuredgrid_latlon.ref
    M       test/testoutput/dirac_spectral_1.ref
    M       test/testoutput/dirac_spectral_no_wind.ref
    M       test/testoutput/ens_mean_variance.ref
    M       test/testoutput/forecast.ref
    M       test/testoutput/parameters_bumpcov.ref
    M       test/testoutput/parameters_bumploc.ref
    M       test/testoutput/process_perts_spectral_no_wind.ref
    M       test/testoutput/rtpp.ref
    
    TESTS CONDUCTED:
    - Tests were conducted on HPC Derecho using HofX and with the MPAS-Workflow for the assimilation of GNSSRO bending angle observations

--- src/mpasjedi/VariableChange/Model2GeoVars/mpasjedi_vc_model2geovars_mod.F90
+++ src/mpasjedi/VariableChange/Model2GeoVars/mpasjedi_vc_model2geovars_mod.F90
@@ -449,22 +449,10 @@ subroutine changevar(self, geom, xm, xg)
 
         case ( var_z ) !-geopotential_height, geopotential heights at midpoint
           ! calculate midpoint geometricZ (unit: m):
-          do iCell = 1, nCells
-            lat = geom%latCell(iCell) * MPAS_JEDI_RAD2DEG_kr !- to Degrees
-            do iLevel = 1, nVertLevels
-               call geometric2geop(real(lat,kind=kind_real), real(geom%height(iLevel,iCell),kind=kind_real), rz)
-               gdata%r2%array(iLevel,iCell)=rz
-            enddo
-          enddo
+          gdata%r2%array(:,1:nCells)=geom%height(:,1:nCells)
 
         case ( var_zi ) !-geopotential_height_levels, geopotential heights at w levels
-          do iCell = 1, nCells
-            lat = geom%latCell(iCell) * MPAS_JEDI_RAD2DEG_kr !- to Degrees
-            do iLevel = 1, nVertLevelsP1
-               call geometric2geop(real(lat,kind=kind_real), real(geom%zgrid(iLevel,iCell),kind=kind_real), rz)
-               gdata%r2%array(iLevel,iCell)=rz
-            enddo
-          enddo
+          gdata%r2%array(:,1:nCells)=geom%zgrid(:,1:nCells)
 
         case ( var_geomz, var_zm ) !-height
           ! calculate midpoint geometricZ (unit: m):
@@ -486,11 +474,7 @@ subroutine changevar(self, geom, xm, xg)
 
 !! begin surface variables
         case ( var_sfc_z ) !-surface_geopotential_height
-          do iCell=1,nCells
-            lat = geom%latCell(iCell) * MPAS_JEDI_RAD2DEG_kr !- to Degrees
-            call geometric2geop(real(lat,kind=kind_real), real(geom%zgrid(1,iCell),kind=kind_real), rz)
-            gdata%r1%array(iCell)=rz
-          enddo
+          gdata%r1%array(1:nCells) = geom%zgrid(1,1:nCells)
 
         case ( var_sfc_geomz ) !-surface_altitude
           gdata%r1%array(1:nCells) = geom%zgrid(1,1:nCells)
